1af1f5b6e701406009f9e7b0a16ba242d70926f0
urjiiko1 <gemachistesfaye36@gmail.com>
2026-01-20 23:59:45 +0300

    update for include two section as well as pass for cv work both on navbar and exprience

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Navbar from "./navbar";
 import Home from "./home";
 import About from "./about";
 import Skill from "./skill";
+import Service from "./service";
+import Experience from "./exprience";
 import Project from "./project";
 import Contact from "./contact";
+import ScrollToTopButton from './scrollTotopbutton';
 import Footer from "./footer";
+
 
 import "./App.css";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [showCVOptions, setShowCVOptions] = useState(false); 
 
   const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
@@ -24,12 +29,27 @@ function App() {
 
   return (
     <main className="bg-gray-100 dark:bg-gray-900 min-h-screen transition-colors duration-300">
-      <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      {}
+      <Navbar
+        isDarkMode={isDarkMode}
+        toggleDarkMode={toggleDarkMode}
+        setShowCVOptions={setShowCVOptions}
+      />
+
       <Home />
       <About />
       <Skill />
+      <Service />
+
+      {}
+      <Experience
+        showCVOptions={showCVOptions}
+        setShowCVOptions={setShowCVOptions}
+      />
+
       <Project />
       <Contact />
+      <ScrollToTopButton />
       <Footer />
     </main>
   );
